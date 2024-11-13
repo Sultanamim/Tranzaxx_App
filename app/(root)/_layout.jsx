1ae62ded79fec942d47cb2b752e98a06ebedeb6e
unknown <humairasultana059@gmail.com>
2024-11-13 11:20:06 +0600

    updated by humaira

--- app/(root)/_layout.jsx
+++ app/(root)/_layout.jsx
@@ -1,17 +1,17 @@
-import { Stack } from "expo-router"
+import { Stack } from "expo-router";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 
 export default function RootLayout() {
-    return (
-        <Stack>
-            <Stack.Screen name="home" options={{ headerShown: false }} />
-            <Stack.Screen name="pricing" options={{ headerShown: false }} />
-            <Stack.Screen name="faq" options={{ headerShown: false }} />
-            <Stack.Screen name="contact" options={{ headerShown: false }} />
-            <Stack.Screen name="ads" options={{ headerShown: false }} />
-            <Stack.Screen name="country" options={{ headerShown: false }} />
-            <Stack.Screen name="advertise" options={{ headerShown: false }} />
-        </Stack>
-    )
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="home" />
+      <Stack.Screen name="pricing" />
+      <Stack.Screen name="faq" />
+      <Stack.Screen name="contact" />
+      <Stack.Screen name="ads" />
+      <Stack.Screen name="country" />
+      <Stack.Screen name="advertise" />
+    </Stack>
+  );
 }
---
+++ constant/newData.js
@@ -0,0 +1,63 @@
+import realState from "../assets/images/listing/real-state.png";
+import automobile from "../assets/images/listing/automobile.png";
+import electronic from "../assets/images/listing/electronics.png";
+import phone from "../assets/images/listing/phone.png";
+import animal from "../assets/images/listing/animal.png";
+import fashion from "../assets/images/listing/cloth.png";
+import jobs from "../assets/images/listing/jobs.png";
+import services from "../assets/images/listing/service.png";
+import books from "../assets/images/listing/booking.png";
+import furniture from "../assets/images/listing/furniture.png";
+import Beauty from "../assets/images/listing/beauty.png";
+import gaming from "../assets/images/listing/gaming.png";
+
+export const productsCategories = [
+  {
+    name: "Real State",
+    img: realState,
+  },
+  {
+    name: "Automobiles",
+    img: automobile,
+  },
+  {
+    name: "Electronics",
+    img: electronic,
+  },
+  {
+    name: "Phones & Tablets",
+    img: phone,
+  },
+  {
+    name: "Animal & Pets",
+    img: animal,
+  },
+  {
+    name: "Clothing & fashion",
+    img: fashion,
+  },
+  {
+    name: "Jobs",
+    img: jobs,
+  },
+  {
+    name: "Services",
+    img: services,
+  },
+  {
+    name: "Books & Learning",
+    img: books,
+  },
+  {
+    name: "Furniture & Appliances",
+    img: furniture,
+  },
+  {
+    name: "Beauty",
+    img: Beauty,
+  },
+  {
+    name: "Gaming",
+    img: gaming,
+  },
+];
